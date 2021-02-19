e9058b9a4bbf648e2ab5cdedc4b416356b8fd9d1
lee jeongmin <danmin20@khu.ac.kr>
2021-02-19 14:40:28 +0900

    feat: add assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const config = require('config');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
+  assetPrefix: isProd ? 'https://www.spaceone.org/' : '',
   serverRuntimeConfig: {
     githubAccessToken: config.get('GITHUB.ACCESS_TOKEN'),
   },
