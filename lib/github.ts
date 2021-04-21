3185b1b9dfd7c1a0a95e65ce038f1b0bd53c81d4
WANJIN <wanjin@megazone.com>
2021-04-21 14:40:51 +0900

    Add try catch to apis

--- lib/github.ts
+++ lib/github.ts
@@ -10,46 +10,56 @@ const BRANCH = 'master';
 const NOTE_PATH = 'release_notes/ko';
 
 const octokit = new Octokit({
-  auth: serverRuntimeConfig.githubAccessToken,
+    auth: serverRuntimeConfig.githubAccessToken,
 });
 
 export const getRootSha = async (): Promise<string> => {
-  try {
-    const { data } = await octokit.request('GET /repos/{owner}/{repo}/branches/{branch}', {
-      owner: OWNER,
-      repo: REPO,
-      branch: BRANCH,
-    });
-    return data.commit.sha;
-  } catch (e) {
-    console.error(e);
-    return '';
-  }
+    try {
+        const { data } = await octokit.request('GET /repos/{owner}/{repo}/branches/{branch}', {
+            owner: OWNER,
+            repo: REPO,
+            branch: BRANCH,
+        });
+        return data.commit.sha;
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
 };
 
 export const getNoteTree = async (sha: string, recursive = true): Promise<string[]> => {
-  const { data } = await octokit.request('GET /repos/{owner}/{repo}/git/trees/{tree_sha}', {
-    owner: OWNER,
-    repo: REPO,
-    tree_sha: sha,
-    recursive: recursive as unknown as string,
-  });
+    try {
+        const { data } = await octokit.request('GET /repos/{owner}/{repo}/git/trees/{tree_sha}', {
+            owner: OWNER,
+            repo: REPO,
+            tree_sha: sha,
+            recursive: recursive as unknown as string,
+        });
 
-  const tree = data.tree.find((d) => d.path === NOTE_PATH);
-  if (tree) {
-    const notes = await getNoteTree(tree.sha, false);
-    return notes;
-  }
+        const tree = data.tree.find((d) => d.path === NOTE_PATH);
+        if (tree) {
+            const notes = await getNoteTree(tree.sha, false);
+            return notes;
+        }
 
-  return data.tree.map((d) => d.path.slice(0, d.path.length - 3));
+        return data.tree.map((d) => d.path.slice(0, d.path.length - 3));
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
 };
 
 export const getData = async (noteVersion): Promise<string> => {
-  const { data } = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
-    owner: OWNER,
-    repo: REPO,
-    path: `${NOTE_PATH}/${noteVersion}.md`,
-  });
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return utf8.decode(base64.decode((data as any).content));
+    try {
+        const { data } = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+            owner: OWNER,
+            repo: REPO,
+            path: `${NOTE_PATH}/${noteVersion}.md`,
+        });
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        return utf8.decode(base64.decode((data as any).content));
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
 };
